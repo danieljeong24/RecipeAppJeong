1473b3c1faf23f2e6c3c4bc0ffce8d346a38524d
Daniel Jeong <89224086+danieljeong24@users.noreply.github.com>
2023-10-10 10:08:27 -0600

    DismissAddRecipe

---
+++ RecipeAppJeong/Detail/AddRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRecipeView.swift
+//  RecipeAppJeong
+//
+//  Created by Daniel Jeong on 10/10/23.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRecipeView()
+}
